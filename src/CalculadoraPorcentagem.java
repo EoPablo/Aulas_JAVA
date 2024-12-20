af38cedb09afb621cdfdacc0ac57170586859b97
Pablo <pablocavalcantee@gmail.com>
2024-12-20 10:13:33 -0300

    Calculadora porcentagem att

--- src/CalculadoraPorcentagem.java
+++ src/CalculadoraPorcentagem.java
@@ -2,13 +2,13 @@ public class CalculadoraPorcentagem {
     public static void main(String[] args) {
         float salario = 4600F;
         if(salario > 4500){
-            double percent = salario * 0.3;
+            float percent = salario * 0.3F;
             String resultadoFormatado = String.format("%.2f", percent);
-            System.out.println("30% é R$ " + resultadoFormatado);
+            System.out.println("30% é → R$ " + resultadoFormatado);
         } else {
-            double percent = salario * 0.15;
+            float percent = salario * 0.15F;
             String resultadoFormatado = String.format("%.2f", percent);
-            System.out.println("15% é R$ " + resultadoFormatado);
+            System.out.println("15% é → R$ " + resultadoFormatado);
         }
     }
 }
